1c01235e03a6e5c9c99b62e0f5d23793bfa280d2
Abhijeet Singh <1398abhisingh911@gmail.com>
2021-06-30 21:38:43 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+print("content-type: text/html")
+print("Access-Control-Allow-Origin: *")
+print()
+
+import cgi, sys ,os
+import cgitb
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+sys.path.insert(0, os.getcwd())
+
+message = None
+
+
+if 'filename' in form:
+    fileitem = form['filename']
+    fn = os.path.basename(fileitem.filename)
+    open('/var/www/cgi-bin/' + fn, 'wb').write(fileitem.file.read())
+    message = 'The file "' + fn + '" was uploaded successfully'
+else:
+    message = 'No file was uploaded'
+
+replyhtml = """
+<html>
+<body>
+<p>%s</p>
+</body>
+</html>
+"""
+print(replyhtml % message)
